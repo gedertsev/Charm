b7ec35d41b1b32d9263d12891a4b6ff20cb2e7d6
gedertsev <gedertsev@gmail.com>
2018-01-10 16:56:09 +0300

    add config.cmake

---
+++ config.Debug.cmake
@@ -0,0 +1,18 @@
+
+set(QMAKE_EXECUTABLE "D:/dev/local/win64-msvc2017/Debug/Qt/5.9.3/bin/qmake.exe" CACHE FILEPATH "" FORCE)
+
+set(Qt5_CMAKE_DIR "D:/dev/local/win64-msvc2017/Debug/Qt/5.9.3/lib/cmake" CACHE PATH "" FORCE)
+set(Qt5Core_DIR ${Qt5_CMAKE_DIR}/Qt5Core CACHE PATH "" FORCE)
+set(Qt5Gui_DIR ${Qt5_CMAKE_DIR}/Qt5Gui CACHE PATH "" FORCE)
+set(Qt5Widgets_DIR ${Qt5_CMAKE_DIR}/Qt5Widgets CACHE PATH "" FORCE)
+set(Qt5Xml_DIR ${Qt5_CMAKE_DIR}/Qt5Xml CACHE PATH "" FORCE)
+set(Qt5Sql_DIR ${Qt5_CMAKE_DIR}/Qt5Sql CACHE PATH "" FORCE)
+set(Qt5Network_DIR ${Qt5_CMAKE_DIR}/Qt5Network CACHE PATH "" FORCE)
+set(Qt5Test_DIR ${Qt5_CMAKE_DIR}/Qt5Test CACHE PATH "" FORCE)
+set(Qt5PrintSupport_DIR ${Qt5_CMAKE_DIR}/Qt5PrintSupport CACHE PATH "" FORCE)
+set(Qt5WinExtras_DIR ${Qt5_CMAKE_DIR}/Qt5WinExtras CACHE PATH "" FORCE)
+
+set(Qt5Keychain_DIR "D:/dev/local/win64-msvc2017/Debug/qtkeychain/0.8.0/lib/cmake/Qt5Keychain" CACHE PATH "" FORCE)
+
+
+
